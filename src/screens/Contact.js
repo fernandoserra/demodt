18d997ac77da5a98c5496640a17f574915cc878e
Fernando Serra <fernando.serra.r@gmail.com>
2021-10-25 17:47:38 -0300

    Se agrego Flatlist con numero de columnas

--- src/screens/Contact.js
+++ src/screens/Contact.js
@@ -1,18 +1,52 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, {useContext} from "react";
+import { View, StyleSheet, Text, FlatList } from "react-native";
+import themeContext from "../context/themeContext";
 
-const Contact = () => {
-  return (
-    <View style={styles.center}>
-      <Text>This is the contact screen</Text>
-    </View>
-  );
+const dataList =[{key:'1'},{key:'2'},{key:'3'},{key:'4'},{key:'5'}]
+const numColumns=2
+const Contact = ({ navigation }) => {
+
+    const theme = useContext(themeContext)
+    
+    renderItem=({item,index})=>{
+      let {itemStyle,itemText} =styles; 
+      return(
+        <View style={itemStyle}>
+          <Text style={itemText}>{item.key}</Text>
+        </View>
+      );
+    };
+
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={dataList}
+          renderItem={this.renderItem}
+          numColumns={numColumns}
+          />
+      </View>
+    );
+
+   
+
 };
 
+/*
+    <View style={[styles.center]}>
+            <Text>This is the contact screen...</Text>
+            
+            <View style={styles.container}>
+              <FlatList
+                data={dataList}
+                renderItem={this.renderItem}
+                />
+            </View>
 
-Contact.navigationOptions={
-    title: 'Add Track'
-};
+        </View>
+
+*/
+
+//, {backgroundColor:theme.background}
 
 const styles = StyleSheet.create({
   center: {
@@ -21,6 +55,21 @@ const styles = StyleSheet.create({
     alignItems: "center",
     textAlign: "center",
   },
+  container:{
+    flex: 1
+  },
+  itemStyle:{
+    backgroundColor:'#3232ff',
+    alignItems:'center',
+    justifyContent:'center',
+    height:100,
+    flex:1,
+    margin:1
+  },
+  itemText:{
+    fontSize:50,
+    color:'#fff'
+  }
 });
 
 export default Contact;
