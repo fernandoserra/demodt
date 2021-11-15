d9ba6ef2f42b0f345291f200c6cd3ddf0a04e73f
Fernando Serra <fernando.serra.r@gmail.com>
2021-11-15 00:18:41 -0300

    Add map -> screen About.js

--- src/screens/About.js
+++ src/screens/About.js
@@ -1,24 +1,61 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import themeContext from "../context/themeContext";
+
+import MapView, { Polyline } from "react-native-maps";
 const About = () => {
 
-    const theme = useContext(themeContext)
+	const theme = useContext(themeContext)
 
-    return (
-        <View style={[styles.center, {backgroundColor:theme.background}]}>
-            <Text style={{ fontFamily:'Roboto-Bold'}}>This is the about screen</Text>
-        </View>
-    );
+	return (
+		<View>
+
+			<View style={styles.container_other}>
+				<MapView
+					initialRegion={{
+						longitude: -70.64971943121438,
+						//latitude:37.33233141 + increment * tenMetersWithDegress
+						latitude: -33.44903690630758,
+						latitudeDelta: 0.04,
+						longitudeDelta: 0.05,
+					}}
+					style={styles.map}
+				>
+
+				</MapView>
+
+			</View>
+		</View>
+
+
+		/*
+			<View style={[styles.center, {backgroundColor:theme.background}]}>
+				<Text style={{ fontFamily:'Roboto-Bold'}}>This is the about screen</Text>
+			</View>
+		*/
+
+	);
 };
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    textAlign: "center",
-  },
+	center: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		textAlign: "center",
+	},
+	container_other: {
+		flex: 1,
+		marginTop: 10,
+		marginHorizontal: 16,
+		marginBottom: 100,
+		borderRadius: 30,
+		padding: 10,
+		backgroundColor: 'white',
+	},
+	map: {
+		height: 300
+	}
 });
 
 export default About;
