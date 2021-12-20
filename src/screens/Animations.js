38177bb61fa0a75418402cb3ab5d7e4cb1b28ea5
Fernando Serra <fernando.serra.r@gmail.com>
2021-12-20 17:54:24 -0300

    Se creo el screen Animations.js

---
+++ src/screens/Animations.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, StyleSheet, Text, FlatList } from "react-native";
+
+const Animations = (() => {
+
+    return (
+        <View style={styles.container}>
+           <Text>Animations</Text>
+        </View>
+    );
+
+});
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+});
+
+export default Animations;
