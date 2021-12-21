fa8ed1a1e9f9ec5be5f52717890471e45262fd9d
Fernando Serra <fernando.serra.r@gmail.com>
2021-12-21 15:19:23 -0300

    Create DemoHeader

---
+++ src/screens/DemoHeader.js
@@ -0,0 +1,39 @@
+import React, { useContext } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import themeContext from "../context/themeContext";
+
+const DemoHeader = () => {
+
+	const theme = useContext(themeContext)
+
+	return (
+		<View>
+			<View style={styles.container_other}>
+                <Text>Demo Header</Text>
+			</View>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({
+	center: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+		textAlign: "center",
+	},
+	container_other: {
+		flex: 1,
+		marginTop: 10,
+		marginHorizontal: 16,
+		marginBottom: 100,
+		borderRadius: 30,
+		padding: 10,
+		backgroundColor: 'white',
+	},
+	map: {
+		height: 300
+	}
+});
+
+export default DemoHeader;
