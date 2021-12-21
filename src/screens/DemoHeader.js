3bbeee2d890eb03d6711ced5ac3a2a0d975d9c4e
Fernando Serra <fernando.serra.r@gmail.com>
2021-12-21 16:08:09 -0300

    Update DemoHeader

--- src/screens/DemoHeader.js
+++ src/screens/DemoHeader.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { View, StyleSheet, Text, Image, TouchableOpacity } from "react-native";
 import themeContext from "../context/themeContext";
+import { Button, Overlay } from 'react-native-elements';
 
 const DemoHeader = () => {
 
@@ -8,32 +9,76 @@ const DemoHeader = () => {
 
 	return (
 		<View>
-			<View style={styles.container_other}>
-                <Text>Demo Header</Text>
-			</View>
+			 <View style={{
+                height: 150,
+                backgroundColor: '#003366',
+                borderBottomLeftRadius: 30,
+                borderBottomEndRadius: 30
+            }}>
+                 <TouchableOpacity
+                    style={{ paddingLeft: 10, paddingTop: 50 }}
+                    onPress={() => {
+                        console.log("Ejecutando")
+                        return (
+                            <Overlay isVisible>
+                                <View style={{width:400, height:400}}>
+                                    <Text style={styles.titleModel}>En estos momentos estamos trabajando para cargar el contenido</Text>
+                                </View>
+                            </Overlay>
+                        );
+                    }}
+                 >
+                     <Text>Overlay</Text>
+
+                 </TouchableOpacity>
+                <Image
+                    style={styles.image}
+                    source={{ uri: "https://cdn.pixabay.com/photo/2020/01/01/18/11/train-4734116_1280.jpg" }}
+                />
+
+            </View>
 		</View>
 	);
+
+    /*
+    
+     <TouchableOpacity
+                    style={{ paddingLeft: 10, paddingTop: 50 }}
+                    onPress={() => {
+                        console.log("Ejecutando Overlay xyz")
+                        return (
+                            <Overlay isVisible>
+                                <View style={{width:400, height:400}}>
+                                    <Text style={styles.titleModel}>En estos momentos estamos trabajando para cargar el contenido</Text>
+                                </View>
+                            </Overlay>
+                        );
+
+                    }}>
+                    <Text> Información </Text>
+                </TouchableOpacity>
+
+    */
 };
 
 const styles = StyleSheet.create({
-	center: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-		textAlign: "center",
-	},
-	container_other: {
-		flex: 1,
-		marginTop: 10,
-		marginHorizontal: 16,
-		marginBottom: 100,
-		borderRadius: 30,
-		padding: 10,
-		backgroundColor: 'white',
-	},
-	map: {
-		height: 300
-	}
+    image: {
+        width: 110,
+        height: 150,
+        margin: 1,
+        position: 'absolute',
+        marginTop: 50,
+        alignSelf: 'center'
+    },
+    titleModel: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        textAlign: 'center'
+    },
+    modal: {
+        margin: 10
+    }
 });
 
 export default DemoHeader;
