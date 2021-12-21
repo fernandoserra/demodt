9d53fd8981da1fa61bc129553dfd6cd7deb28141
Fernando Serra <fernando.serra.r@gmail.com>
2021-12-21 16:31:05 -0300

    Update DemoHeader

--- src/screens/DemoHeader.js
+++ src/screens/DemoHeader.js
@@ -1,44 +1,64 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { View, StyleSheet, Text, Image, TouchableOpacity } from "react-native";
 import themeContext from "../context/themeContext";
 import { Button, Overlay } from 'react-native-elements';
 
 const DemoHeader = () => {
 
-	const theme = useContext(themeContext)
+    const theme = useContext(themeContext)
 
-	return (
-		<View>
-			 <View style={{
+    const [visible, setVisible] = useState(false);
+
+    return (
+        <View>
+            <View style={{
                 height: 150,
                 backgroundColor: '#003366',
                 borderBottomLeftRadius: 30,
                 borderBottomEndRadius: 30
             }}>
-                 <TouchableOpacity
+                <TouchableOpacity
                     style={{ paddingLeft: 10, paddingTop: 50 }}
                     onPress={() => {
+                        setVisible(!visible);
                         console.log("Ejecutando")
-                        return (
-                            <Overlay isVisible>
-                                <View style={{width:400, height:400}}>
-                                    <Text style={styles.titleModel}>En estos momentos estamos trabajando para cargar el contenido</Text>
-                                </View>
-                            </Overlay>
-                        );
-                    }}
-                 >
-                     <Text>Overlay</Text>
 
-                 </TouchableOpacity>
+                    }}
+
+
+                >
+                    <Text>Overlay</Text>
+
+                </TouchableOpacity>
+
+
                 <Image
                     style={styles.image}
                     source={{ uri: "https://cdn.pixabay.com/photo/2020/01/01/18/11/train-4734116_1280.jpg" }}
                 />
 
             </View>
-		</View>
-	);
+
+            <View>
+                <Text>Contenido {JSON.stringify(visible)}</Text>
+
+                <Overlay isVisible={visible}>
+                    <View style={styles.modal}>
+                        <Text style={styles.titleModel}>En estos momentos estamos trabajando para cargar el contenido</Text>
+                        <Button
+                            title="Cerrar"
+                            type="outline"
+                            onPress={() => {
+                                setVisible(!visible);
+                                console.log("Ejecutando boton cerrar")
+
+                            }}
+                        />
+                    </View>
+                </Overlay>
+            </View>
+        </View>
+    );
 
     /*
     
