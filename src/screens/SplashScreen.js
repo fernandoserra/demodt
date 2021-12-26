165d05010f6e2b823547a0684c4b332a7c2f2035
Fernando Serra <fernando.serra.r@gmail.com>
2021-12-25 23:29:40 -0300

    Create SplashScreen

---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const SplashScreen = ({ navigation }) => {
+    return(
+        <View>
+            <Text>SplashScreen</Text>
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default SplashScreen;
