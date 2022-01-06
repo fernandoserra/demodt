9e3828e31eb7137dff77365f4e56a12efe0c027c
Fernando Serra <fernando.serra.r@gmail.com>
2022-01-06 15:09:59 -0300

    Update BdScreen

--- src/screens/BdScreen.js
+++ src/screens/BdScreen.js
@@ -1,9 +1,9 @@
 /* eslint-disable prettier/prettier */
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
 
 const BdScreem = () => {
-    return(
+    return (
         <View>
             <Text>BdScreem</Text>
         </View>
