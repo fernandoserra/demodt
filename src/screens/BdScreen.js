ba82b608681470dc448479d8e50e3ea09f9a849e
Fernando Serra <fernando.serra.r@gmail.com>
2022-01-08 23:01:41 -0300

    Add AsyncStorege.getAllkey      useEffect()      FlatList

--- src/screens/BdScreen.js
+++ src/screens/BdScreen.js
@@ -1,28 +1,80 @@
-import React, { useContext,useState } from 'react';
-import { View, StyleSheet, Text,Alert } from 'react-native';
-import { Input, Button } from "react-native-elements";
+import React, { useEffect, useContext, useState } from 'react';
+import { View, StyleSheet, Text, Alert, TouchableOpacity,FlatList } from 'react-native';
+import { Input, Button, Card } from "react-native-elements";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import Spacer from '../components/Spacer';
 
-const BdScreem = () => {
-    
+const BdScreem = ({ navigation }) => {
+
     const [name, setName] = useState('');
-    
-    const addBD = async () =>{
+    const [itemsShow, setItemsShow] = useState([]);
+
+    const addBD = async () => {
         var dateNow = Date.now();
-        console.log(` Valor a almacenar: ${name}  id: ${dateNow}` )
+        console.log(` Valor a almacenar: ${name}  id: ${dateNow}`)
         await AsyncStorage.setItem(dateNow.toString(), `${name}`);
         Alert.alert("Se almaceno la información", '')
+        getAllKeys()
     };
+
+    const getAllKeys = async () => {
+        try {
+            const allKeys = await AsyncStorage.getAllKeys();
+            const items = await AsyncStorage.multiGet(allKeys)
+            setItemsShow(items)
+            return allKeys
+        } catch (err) {
+            throw Error(err);
+        }
+    }
+
+    useEffect(() => {
+        const unsubscribe = navigation.addListener('focus', () => {
+            // do something
+            console.log("Ejecutando useEffect()  Focus" + Date.now())
+            getAllKeys()
+        });
+
+        const unsubscribe2 = navigation.addListener('blur', () => {
+            // do something
+            console.log("Ejecutando useEffect()  blur" + Date.now())
+        });
+    }, [navigation]);
+
 
     return (
         <View>
-            <Input value={name} onChangeText={setName}  placeholder="Agregar Tarea" />
-            <Spacer>
-                <Button title="Añadir" onPress={addBD} />
-            </Spacer>
+
+            <View>
+                <Input value={name} onChangeText={setName} placeholder="Agregar Tarea" />
+                <Spacer>
+                    <Button title="Añadir" onPress={addBD} />
+                </Spacer>
+            </View>
+
+            <View>
+                <FlatList
+                    data={itemsShow}
+                    renderItem={({ item, index }) => {
+                        return (
+                            <TouchableOpacity onPress={() => {
+                                console.log('touchable')
+                            }}>
+                                <Card containerStyle={{ padding: 0 }} >
+                                    <View >
+                                        <Text>{item[1]}</Text>
+                                    </View>
+                                </Card>
+                            </TouchableOpacity>
+                        )
+                    }
+                    }
+                />
+
+            </View>
         </View>
+
     );
 };
 
