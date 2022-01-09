ccb5f27878319381a6f41a47139e245c4e56227f
Fernando Serra <fernando.serra.r@gmail.com>
2022-01-08 22:47:00 -0300

    Add method addBD()

--- src/screens/BdScreen.js
+++ src/screens/BdScreen.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useContext,useState } from 'react';
+import { View, StyleSheet, Text,Alert } from 'react-native';
 import { Input, Button } from "react-native-elements";
-import { View, StyleSheet, Text } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import Spacer from '../components/Spacer';
 
 const BdScreem = () => {
+    
+    const [name, setName] = useState('');
+    
+    const addBD = async () =>{
+        var dateNow = Date.now();
+        console.log(` Valor a almacenar: ${name}  id: ${dateNow}` )
+        await AsyncStorage.setItem(dateNow.toString(), `${name}`);
+        Alert.alert("Se almaceno la información", '')
+    };
+
     return (
         <View>
-            <Input placeholder="Agregar Tarea" />
+            <Input value={name} onChangeText={setName}  placeholder="Agregar Tarea" />
             <Spacer>
-                <Button title="Añadir" />
+                <Button title="Añadir" onPress={addBD} />
             </Spacer>
         </View>
     );
