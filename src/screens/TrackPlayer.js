84555ec8dfc41934d8739db7cae8e74945dc82cc
Fernando Serra <fernando.serra.r@gmail.com>
2022-01-17 20:14:17 -0300

    Create screen TrackPlayer

---
+++ src/screens/TrackPlayer.js
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import themeContext from "../context/themeContext";
+
+const TrackPlayer = () => {
+
+    return(
+        <View>
+            <Text>TrackPlayer</Text>
+        </View>
+    );
+
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default TrackPlayer;
