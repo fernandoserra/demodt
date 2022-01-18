394e8a81c0baac6f80ee7f134891d3b869ad776a
Fernando Serra <fernando.serra.r@gmail.com>
2022-01-17 23:23:48 -0300

    Update service.js

---
+++ ios/dummy.swift
@@ -0,0 +1,8 @@
+//
+//  dummy.swift
+//  demodt
+//
+//  Created by Fernando Serra on 17-01-22.
+//
+
+import Foundation
--- service.js
+++ service.js
@@ -1,10 +1,9 @@
-import TrackPlayer from "react-native-track-player";
+import TrackPlayer from 'react-native-track-player';
 
-module.exports = async function(){
-    TrackPlayer.addEventListener('remote-play',()=>
-    TrackPlayer.play());
+module.exports = async function () {
 
-    TrackPlayer.addEventListener('remote-pause',()=>
-    TrackPlayer.pause);
+    TrackPlayer.addEventListener('remote-play', () => TrackPlayer.play());
+
+    TrackPlayer.addEventListener('remote-pause', () => TrackPlayer.pause());
 
 };
