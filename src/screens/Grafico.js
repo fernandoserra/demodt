d1055ee1eb672d88f2169e7230a33021830dc151
Fernando Serra <fernando.serra.r@gmail.com>
2022-01-27 19:35:55 -0300

    Update package

--- src/screens/Grafico.js
+++ src/screens/Grafico.js
@@ -2,33 +2,27 @@ import React, { useContext } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import themeContext from "../context/themeContext";
 
+
+
 const Grafico = () => {
-	const theme = useContext(themeContext)
-	return (
-		<View>
-			<View style={styles.container_other}>
-                <Text>Grafico</Text>
-			</View>
-		</View>
-	);
+    const theme = useContext(themeContext)
+
+   
+
+    return (
+        <View style={styles.container}>
+           <Text> Grafico</Text>
+        </View>
+    );
 };
 
 const styles = StyleSheet.create({
-	center: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-		textAlign: "center",
-	},
-	container_other: {
-		flex: 1,
-		marginTop: 10,
-		marginHorizontal: 16,
-		marginBottom: 100,
-		borderRadius: 30,
-		padding: 10,
-		backgroundColor: 'white',
-	}
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f7f7f7',
+    },
 });
 
 export default Grafico;
