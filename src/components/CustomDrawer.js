b283ec16518f3b9372ef54fb1a5bd4ba5949854f
Fernando Serra <fernando.serra.r@gmail.com>
2022-01-26 22:01:21 -0300

    Update CustomDrawer

---
+++ src/components/CustomDrawer.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  ImageBackground,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import {
+  DrawerContentScrollView,
+  DrawerItemList,
+} from '@react-navigation/drawer';
+
+const CustomDrawer = props => {
+    return (  
+        <View></View>
+    );
+};
+
+export default CustomDrawer;
