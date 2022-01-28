b869647e658dfc65d15cc2d9d2d058469968139d
Fernando Serra <fernando.serra.r@gmail.com>
2022-01-28 11:59:32 -0300

    Update Grafico.js

--- src/screens/Grafico.js
+++ src/screens/Grafico.js
@@ -2,14 +2,27 @@ import React, { useContext } from "react";
 import { View, StyleSheet, Text } from "react-native";
 import themeContext from "../context/themeContext";
 
+import { VictoryBar, VictoryChart, VictoryTheme } from "victory-native";
+
+
+//https://formidable.com/open-source/victory/docs/victory-pie
+
 const Grafico = () => {
     const theme = useContext(themeContext)
 
-   
+    const data = [
+        { quarter: 1, earnings: 13000 },
+        { quarter: 2, earnings: 16500 },
+        { quarter: 3, earnings: 14250 },
+        { quarter: 4, earnings: 19000 }
+    ];
+
 
     return (
         <View style={styles.container}>
-           <Text> Grafico</Text>
+            <VictoryChart width={350} theme={VictoryTheme.material}>
+                <VictoryBar data={data} x="quarter" y="earnings" />
+            </VictoryChart>
         </View>
     );
 };
@@ -17,10 +30,10 @@ const Grafico = () => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#f7f7f7',
-    },
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#f5fcff"
+    }
 });
 
 export default Grafico;
