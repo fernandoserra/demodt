c1ef549476424e3d8d03d65434aeb4f841437620
Fernando Serra <fernando.serra.r@gmail.com>
2022-01-28 12:08:48 -0300

    Update ScreenSVG

---
+++ src/screens/ScreenSVG.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import { View, StyleSheet, Text } from "react-native";
+
+const ScreenSVG = ({ navigation }) => {
+    return (
+        <View>
+            <Text>ScreenSVG</Text>
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+
+});
+
+export default ScreenSVG;
