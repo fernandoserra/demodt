ac102c88f7fe0b4914ad3eac784add17e44c5590
Fernando Serra <fernando.serra.r@gmail.com>
2022-01-31 20:37:40 -0300

    Update project

--- src/screens/ScreenSVG.js
+++ src/screens/ScreenSVG.js
@@ -1,10 +1,14 @@
 import React, { useContext } from 'react';
 import { View, StyleSheet, Text } from "react-native";
+import Logo from '../images/react-native-logo.svg'
 
 const ScreenSVG = ({ navigation }) => {
     return (
         <View>
             <Text>ScreenSVG</Text>
+            <View>
+            <Logo width={120} height={40} />
+            </View>
         </View>
     );
 }
