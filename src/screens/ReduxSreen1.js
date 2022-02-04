ecd318c3b901c1007704d0e9f45867f71462d5d1
Fernando Serra <fernando.serra.r@gmail.com>
2022-02-04 12:02:19 -0300

    Create ReduxScreen1, ReduxScreen2

---
+++ src/screens/ReduxSreen1.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Text } from "react-native";
+import { useSelector, useDispatch } from 'react-redux';
+import { getCities } from '../redux/actions';
+
+const ReduxScreen1 = ({ navigation }) => {
+
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default ReduxScreen1;
