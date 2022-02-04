2e360374b1beb00fe1861ee2533663ed42406796
Fernando Serra <fernando.serra.r@gmail.com>
2022-02-04 12:29:08 -0300

    Update actions

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -2,6 +2,7 @@ export const SET_USER_NAME = 'SET_USER_NAME';
 export const SET_USER_AGE = 'SET_USER_AGE';
 export const INCREASE_AGE = 'INCREASE_AGE';
 export const GET_CITIES = 'GET_CITIES';
+export const GET_PLACEHOLDER = 'GET_PLACEHOLDER';
 
 const API_URL  = 'https://mocki.io/v1/48419bdb-1d76-45a1-89cb-3ac3fcc7f6ca';
 const API_URL2 = 'https://jsonplaceholder.typicode.com/todos/1';
@@ -48,7 +49,7 @@ export const getJsonplaceholder= () => {
             console.log("=====")
             if (json) {
                 dispatch({
-                    type: GET_CITIES,
+                    type: GET_PLACEHOLDER,
                     payload: json
                 });
             } else {
