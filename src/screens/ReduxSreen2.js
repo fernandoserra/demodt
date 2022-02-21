2fa4d83926a54bf425d896949eeb58048beddaa4
Fernando Serra <fernando.serra.r@gmail.com>
2022-02-21 20:00:14 -0300

    Update navigation

--- src/screens/ReduxSreen2.js
+++ src/screens/ReduxSreen2.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Text } from "react-native";
+import { Button } from 'react-native-elements';
 import { useSelector, useDispatch } from 'react-redux';
 import { getJsonplaceholder } from '../redux/actions';
 
@@ -18,6 +19,12 @@ const ReduxScreen2 = ({ navigation }) => {
     return (
         <View>
             <Text>{JSON.stringify(placeholder)}</Text>
+
+            <View>
+                <Button title={"Salir"}  onPress={() => {
+                     navigation.navigate("Contact")
+                }}/>
+            </View>
         </View>
     );
 
