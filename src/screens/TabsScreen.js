f4784fdeaad2ffbec16e407a958acd2da6e41561
Fernando Serra <fernando.serra.r@gmail.com>
2022-03-04 11:55:49 -0300

    Add create TabsScreen

---
+++ src/screens/TabsScreen.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import { View, StyleSheet, Text } from 'react-native';
+import themeContext from '../context/themeContext';
+
+
+const TabsScreen = () => {
+    const theme = useContext(themeContext)
+    return (
+        <View>
+            <Text>TabsScreen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
+
+export default TabsScreen
