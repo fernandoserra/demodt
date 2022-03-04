bfe5589b24efbab30f11d4ff011d3d6fccb786e6
Fernando Serra <fernando.serra.r@gmail.com>
2022-03-04 12:01:55 -0300

    add DrawerNavigator

--- src/navigations/DrawerNavigator.js
+++ src/navigations/DrawerNavigator.js
@@ -5,7 +5,7 @@ import { ContactStackNavigator } from "./StackNavigator";
 import { AnimationStackNavigator } from "./StackNavigator";
 import { DemoHeaderStackNavigator } from "./StackNavigator";
 import { VideoStackNavigator } from "./StackNavigator";
-import { BdStackNavigator, TrackPlayerNavigator, GraficoNavigator, SVGNavigator, Redux1Navigator, Redux2Navigator,MenuTabNavigator } from "./StackNavigator";
+import { BdStackNavigator, TrackPlayerNavigator, GraficoNavigator, SVGNavigator, Redux1Navigator, Redux2Navigator,MenuTabNavigator,TabsNavigator } from "./StackNavigator";
 import TabNavigator from "./TabNavigator";
 import strings from "../values/strings";
 import CustomDrawer from "../components/CustomDrawer";
@@ -32,6 +32,7 @@ const DrawerNavigator = () => {
             <Drawer.Screen name="Redux1" component={Redux1Navigator} />
             <Drawer.Screen name="Redux2" component={Redux2Navigator} />
             <Drawer.Screen name="Menu Tabs" component={MenuTabNavigator} />
+            <Drawer.Screen name="Tabs" component={TabsNavigator} />
     </Drawer.Navigator>
   );
 }
