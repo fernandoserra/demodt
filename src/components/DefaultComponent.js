dacfc71d2325b69cfa7534b467957b40c6873c66
Fernando Serra <fernando.serra.r@gmail.com>
2022-04-05 22:40:12 -0400

    Update DefaulComponent

---
+++ src/components/DefaultComponent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native';
+
+
+export default DefaultComponent = () => {
+    return (
+        <View>
+            <Text>Datos </Text>
+        </View>
+    )
+}
