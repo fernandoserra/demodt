8749ae853fdf6f74c7679164d03dcf930aa4b627
Fernando Serra <fernando.serra.r@gmail.com>
2022-04-07 13:39:40 -0400

    Update ModalPickerScreen

--- src/screens/ModalPicker/ModalPickerScreen.js
+++ src/screens/ModalPicker/ModalPickerScreen.js
@@ -1,12 +1,57 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity, SafeAreaView, Modal } from 'react-native'
+import ModalPicker from './ModalPicker'
 
 const ModalPickerScreen = () => {
-  return (
-    <View>
-      <Text>ModalPickerScreen</Text>
-    </View>
-  )
+    const [chooseData, setChooseData] = useState('Select item...')
+    const [isModalVisible, setIsModalVisible] = useState(false)
+
+    const changeModalVisibility = (bool) =>{
+        setIsModalVisible(bool)
+    }
+
+    const setData = (option) =>{
+        setChooseData(option)
+    }
+    return (
+        <SafeAreaView style={styles.container}>
+            <TouchableOpacity 
+                onPress={()=> changeModalVisibility(true)}
+                style={styles.touchableOpacity}>
+                <Text style={styles.text}>{chooseData}</Text>
+            </TouchableOpacity>
+            <Modal
+                transparent={true}
+                animationType='fade'
+                visible={isModalVisible}
+                nRequestClose={()=> changeModalVisibility(false)}
+            >
+                <ModalPicker 
+                    changeModalVisibility={changeModalVisibility} 
+                    setData={setData}
+                />
+            </Modal>
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#003366',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20
+    },
+    text: {
+        marginVertical: 20,
+        fontSize: 25
+    },
+    touchableOpacity: {
+        backgroundColor: 'orange',
+        alignSelf: 'stretch',
+        paddingHorizontal: 20
+    }
+})
 
 export default ModalPickerScreen
