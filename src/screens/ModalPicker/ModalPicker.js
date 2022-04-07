5ba0a647ee613928512bb9d63fbf576221c0cc3e
Fernando Serra <fernando.serra.r@gmail.com>
2022-04-07 13:39:11 -0400

    Update ModalPicker

---
+++ src/screens/ModalPicker/ModalPicker.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, Dimensions, ScrollView } from 'react-native'
+
+const OPTIONS = ['red', 'blue', 'yellow', 'green', 'orange', 'white', 'purple']
+const WIDTH = Dimensions.get('window').width;
+const HEIGHT = Dimensions.get('window').height;
+
+const ModalPicker = (props) => {
+
+    const onPressItem = (option) => {
+        props.changeModalVisibility(false);
+        props.setData(option);
+    }
+
+    const option = OPTIONS.map((item, index) => {
+        return (
+            <TouchableOpacity
+                style={styles.option}
+                key={index}
+                onPress={() => onPressItem(item)}
+            >
+                <Text style={styles.text}>
+                    {item}
+                </Text>
+
+            </TouchableOpacity>
+        )
+    })
+
+    return (
+        <TouchableOpacity
+            onPress={() => props.changeModalVisibility(false)}
+            style={styles.container}>
+            <View style={[styles.modal, { width: WIDTH - 20, height: HEIGHT / 2 }]}>
+                <ScrollView>
+                    {option}
+                </ScrollView>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+export default ModalPicker
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    modal: {
+        backgroundColor: 'white',
+        borderRadius: 10
+    },
+    option: {
+        alignItems: 'flex-start'
+    },
+    text: {
+        margin: 20,
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+})
