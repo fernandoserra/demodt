c7987e1d65e5aba3b2d38d4868f0c1ca0a9843ab
Fernando Serra <fernando.serra.r@gmail.com>
2022-04-07 11:58:20 -0400

    Create ModalPickerScreen

---
+++ src/screens/ModalPicker/ModalPickerScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const ModalPickerScreen = () => {
+  return (
+    <View>
+      <Text>ModalPickerScreen</Text>
+    </View>
+  )
+}
+
+export default ModalPickerScreen
