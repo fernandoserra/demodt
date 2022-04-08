4ecc606cbc3dd57528b884be39f179ed7823ad68
Fernando Serra <fernando.serra.r@gmail.com>
2022-04-08 10:45:15 -0400

    Create FormScreen

---
+++ src/screens/FormScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FormScreen = () => {
+  return (
+    <View>
+      <Text>FormScreen</Text>
+    </View>
+  )
+}
+
+export default FormScreen
+
+const styles = StyleSheet.create({})
