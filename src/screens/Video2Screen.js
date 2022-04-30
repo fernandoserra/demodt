1cabaa43fdaaa2c73091170c94d29b49e5c2311a
Fernando Serra <fernando.serra.r@gmail.com>
2022-04-30 19:27:20 -0400

    Update Video2Screen

---
+++ src/screens/Video2Screen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Video2Screen = () => {
+  return (
+    <View>
+      <Text>Video2Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default Video2Screen
